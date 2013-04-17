26bbb9f59a39450ef544b9b15e1262fb4e66a23d
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2013-04-17 18:55:25 +0400

    +1

--- defaultres.go
+++ defaultres.go
@@ -14,6 +14,7 @@ const (
 )
 
 type DefaultResourceImpl struct {
+	readRawFunc     func(string, *http.Request, time.Time) ([]byte, error)
 	Parent_         Resource
 	PathSegment_    string
 	Children        map[string]Resource
@@ -23,13 +24,6 @@ type DefaultResourceImpl struct {
 	Gzip            bool
 	CacheControl_   string
 	IsCollection_   bool
-
-	etagFunc    func() string
-	expiresFunc func() time.Time
-	readFunc    func(string, *http.Request, time.Time) ([]byte, error)
-	updateFunc  func(*http.Request, map[string]interface{}, time.Time) error
-	createFunc  func(*http.Request, map[string]interface{}, time.Time) (Resource, error)
-	deleteFunc  func(string, map[string]interface{}, time.Time) error
 }
 
 func NewDefaultResourceImpl(parent Resource, pathSegment string) *DefaultResourceImpl {
@@ -44,43 +38,6 @@ func NewDefaultResourceImpl(parent Resource, pathSegment string) *DefaultResourc
 }
 
 func (d *DefaultResourceImpl) SetDelegate(del interface{}) {
-	d.setETagDelegate(del)
-	d.setExpiresDelegate(del)
-	d.setReadDelegate(del)
-	d.setUpdateDelegate(del)
-	d.setCreateDelegate(del)
-	d.setDeleteDelegate(del)
-}
-
-func (d *DefaultResourceImpl) setETagDelegate(del interface{}) {
-	if i, ok := del.(interface {
-		ETag() string
-	}); ok {
-		d.etagFunc = func() string {
-			return i.ETag()
-		}
-	} else {
-		d.etagFunc = func() string {
-			return ""
-		}
-	}
-}
-
-func (d *DefaultResourceImpl) setExpiresDelegate(del interface{}) {
-	if i, ok := del.(interface {
-		Expires() time.Time
-	}); ok {
-		d.expiresFunc = func() time.Time {
-			return i.Expires()
-		}
-	} else {
-		d.expiresFunc = func() time.Time {
-			return time.Time{}
-		}
-	}
-}
-
-func (d *DefaultResourceImpl) setReadDelegate(del interface{}) {
 	if i, ok := del.(interface {
 		Data(string, *http.Request, time.Time) (interface{}, error)
 	}); ok {
@@ -88,7 +45,7 @@ func (d *DefaultResourceImpl) setReadDelegate(del interface{}) {
 			panic("Resource has Data function but non-JSON Content-Type")
 		}
 
-		d.readFunc = func(prefix string, r *http.Request, t time.Time) ([]byte, error) {
+		d.readRawFunc = func(prefix string, r *http.Request, t time.Time) ([]byte, error) {
 			data, err := i.Data(prefix, r, t)
 			if err != nil {
 				return nil, err
@@ -97,47 +54,17 @@ func (d *DefaultResourceImpl) setReadDelegate(del interface{}) {
 		}
 	} else {
 		ri, rok := del.(interface {
-			Read(string, *http.Request, time.Time) ([]byte, error)
+			ReadRaw(string, *http.Request, time.Time) ([]byte, error)
 		})
 		if rok {
-			d.readFunc = func(prefix string, r *http.Request, t time.Time) ([]byte, error) {
-				return ri.Read(prefix, r, t)
+			d.readRawFunc = func(prefix string, r *http.Request, t time.Time) ([]byte, error) {
+				return ri.ReadRaw(prefix, r, t)
 			}
 		}
 	}
 }
 
-func (d *DefaultResourceImpl) setUpdateDelegate(del interface{}) {
-	i, ok := del.(interface {
-		Update(*http.Request, map[string]interface{}, time.Time) error
-	})
-	if ok {
-		d.updateFunc = func(r *http.Request, body map[string]interface{}, t time.Time) error {
-			return i.Update(r, body, t)
-		}
-	}
-}
-
-func (d *DefaultResourceImpl) setCreateDelegate(del interface{}) {
-	i, ok := del.(interface {
-		Create(*http.Request, map[string]interface{}, time.Time) (Resource, error)
-	})
-	if ok {
-		d.createFunc = func(r *http.Request, body map[string]interface{}, t time.Time) (Resource, error) {
-			return i.Create(r, body, t)
-		}
-	}
-}
-
-func (d *DefaultResourceImpl) setDeleteDelegate(del interface{}) {
-	i, ok := del.(interface {
-		Delete(string, map[string]interface{}, time.Time) error
-	})
-	if ok {
-		d.deleteFunc = func(s string, body map[string]interface{}, t time.Time) error {
-			return i.Delete(s, body, t)
-		}
-	}
+func (d *DefaultResourceImpl) setReadDelegate(del interface{}) {
 }
 
 func (d *DefaultResourceImpl) AddAction(action string, f func(*http.Request, map[string]interface{}, time.Time) error) {
@@ -169,11 +96,11 @@ func (d *DefaultResourceImpl) AllowedActions() []string {
 }
 
 func (d *DefaultResourceImpl) ETag() string {
-	return d.etagFunc()
+	return ""
 }
 
 func (d *DefaultResourceImpl) Expires() time.Time {
-	return d.expiresFunc()
+	return time.Time{}
 }
 
 func (d *DefaultResourceImpl) CacheControl() string {
@@ -185,7 +112,7 @@ func (d *DefaultResourceImpl) ContentType() string {
 }
 
 func (d *DefaultResourceImpl) Read(urlPrefix string, w http.ResponseWriter, r *http.Request, t time.Time) {
-	data, err := d.readFunc(urlPrefix, r, t)
+	data, err := d.readRawFunc(urlPrefix, r, t)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -205,7 +132,7 @@ func (d *DefaultResourceImpl) Read(urlPrefix string, w http.ResponseWriter, r *h
 }
 
 func (d *DefaultResourceImpl) Update(r *http.Request, body map[string]interface{}, t time.Time) error {
-	return d.updateFunc(r, body, t)
+	panic("Not implemented")
 }
 
 func (d *DefaultResourceImpl) Do(action string, r *http.Request, body map[string]interface{}, t time.Time) error {
@@ -221,9 +148,9 @@ func (d *DefaultResourceImpl) IsCollection() bool {
 }
 
 func (d *DefaultResourceImpl) Create(r *http.Request, body map[string]interface{}, t time.Time) (Resource, error) {
-	return d.createFunc(r, body, t)
+	panic("Not implemented")
 }
 
 func (d *DefaultResourceImpl) Delete(s string, body map[string]interface{}, t time.Time) error {
-	return d.deleteFunc(s, body, t)
+	panic("Not implemented")
 }

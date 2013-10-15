bd32dc0c3cf8700a6e32fb027b20efbda05104e6
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2013-10-15 20:42:24 +0400

    handlers: add RedirectToNonWWWHandler

---
+++ handlers/nonwww.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+type (
+	RedirectToNonWWWHandler struct {
+		http.Handler
+	}
+)
+
+func (h RedirectToNonWWWHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	u := *r.URL
+	if u.Host == "" {
+		u.Host = r.Host
+	}
+
+	if !strings.HasPrefix(u.Host, "www.") {
+		h.ServeHTTP(w, r)
+	} else {
+		if r.TLS == nil {
+			u.Scheme = "http"
+		} else {
+			u.Scheme = "https"
+		}
+
+		u.Host = strings.Replace(u.Host, "www.", "", 1)
+		w.Header().Set("Location", u.String())
+		w.WriteHeader(http.StatusMovedPermanently)
+	}
+}

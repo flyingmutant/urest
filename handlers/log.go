33aecbdd267639b6f16e393d3ab4763ce788fbfa
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2013-10-16 16:35:40 +0400

    handlers/log: colors from git + ability to customize logger

--- handlers/log.go
+++ handlers/log.go
@@ -4,63 +4,93 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"runtime"
 	"time"
 )
 
 type (
-	LoggingHandler struct {
-		http.Handler
+	loggingHandler struct {
+		h http.Handler
+		l *log.Logger
 	}
 )
 
 const (
-	color_RESET     = "\x1b[0m"
-	color_FG_RED    = "\x1b[31m"
-	color_FG_GREEN  = "\x1b[32m"
-	color_FG_YELLOW = "\x1b[33m"
-	color_FG_CYAN   = "\x1b[36m"
+	// https://github.com/git/git/blob/master/color.h
+	_COLOR_NORMAL       = ""
+	_COLOR_RESET        = "\033[m"
+	_COLOR_BOLD         = "\033[1m"
+	_COLOR_RED          = "\033[31m"
+	_COLOR_GREEN        = "\033[32m"
+	_COLOR_YELLOW       = "\033[33m"
+	_COLOR_BLUE         = "\033[34m"
+	_COLOR_MAGENTA      = "\033[35m"
+	_COLOR_CYAN         = "\033[36m"
+	_COLOR_BOLD_RED     = "\033[1;31m"
+	_COLOR_BOLD_GREEN   = "\033[1;32m"
+	_COLOR_BOLD_YELLOW  = "\033[1;33m"
+	_COLOR_BOLD_BLUE    = "\033[1;34m"
+	_COLOR_BOLD_MAGENTA = "\033[1;35m"
+	_COLOR_BOLD_CYAN    = "\033[1;36m"
+	_COLOR_BG_RED       = "\033[41m"
+	_COLOR_BG_GREEN     = "\033[42m"
+	_COLOR_BG_YELLOW    = "\033[43m"
+	_COLOR_BG_BLUE      = "\033[44m"
+	_COLOR_BG_MAGENTA   = "\033[45m"
+	_COLOR_BG_CYAN      = "\033[46m"
 )
 
-func (h LoggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func NewLoggingHandler(h http.Handler, l *log.Logger) *loggingHandler {
+	if l == nil {
+		l = log.New(os.Stdout, "HTTP ", 0)
+	}
+
+	return &loggingHandler{
+		h: h,
+		l: l,
+	}
+}
+
+func (h *loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.RequestURI == "" {
 		// ignore "faked" requests
-		h.Handler.ServeHTTP(w, r)
+		h.h.ServeHTTP(w, r)
 		return
 	}
 
 	start := time.Now()
 	tw := &TransparentResponseWriter{w, http.StatusOK, 0}
 
-	h.Handler.ServeHTTP(tw, r)
+	h.h.ServeHTTP(tw, r)
 
 	h.log(start, tw, r)
 }
 
-func (h LoggingHandler) log(start time.Time, tw *TransparentResponseWriter, r *http.Request) {
+func (h *loggingHandler) log(start time.Time, tw *TransparentResponseWriter, r *http.Request) {
 	dt := time.Now().Sub(start)
-	dtC := colored(fmt.Sprintf("%v", dt), color_FG_CYAN)
+	dtC := colored(fmt.Sprintf("%v", dt), _COLOR_CYAN)
 
 	statusC := fmt.Sprintf("%v", tw.Status)
 	if tw.Success() {
-		statusC = colored(statusC, color_FG_GREEN)
+		statusC = colored(statusC, _COLOR_GREEN)
 	} else if tw.Status >= 400 {
-		statusC = colored(statusC, color_FG_RED)
+		statusC = colored(statusC, _COLOR_RED)
 	}
 
-	methodC := colored(r.Method, color_FG_YELLOW)
+	methodC := colored(r.Method, _COLOR_BLUE)
 
 	sizeS := ""
 	if tw.Size != 0 {
 		sizeS = fmt.Sprintf(", %v bytes", tw.Size)
 	}
 
-	log.Printf("[%v] %v %v (%v%v)", statusC, methodC, r.RequestURI, dtC, sizeS)
+	h.l.Printf("[%v] %v %v (%v%v)", statusC, methodC, colored(r.RequestURI, _COLOR_BOLD), dtC, sizeS)
 }
 
 func colored(s string, color string) string {
 	if runtime.GOOS == "windows" {
 		return s
 	}
-	return color + s + color_RESET
+	return color + s + _COLOR_RESET
 }

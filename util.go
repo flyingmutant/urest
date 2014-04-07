72c3d8efa870aaa9ea171bd29d5fe02f6c8b6860
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2014-04-07 16:48:44 +0400

    helpers: add FeatureFlagPresent()

--- util.go
+++ util.go
@@ -2,6 +2,7 @@ package urest
 
 import (
 	"net/http"
+	"strings"
 )
 
 const (
@@ -10,4 +11,8 @@ const (
 
 func IsSafeRequest(r *http.Request) bool {
 	return r.Method == "GET" || r.Method == "HEAD" || r.Method == "OPTIONS" || r.Method == "TRACE"
+}
+
+func FeatureFlagPresent(r *http.Request, featureFlagHeader string, featureFlag string) bool {
+	return strings.Contains(r.Header.Get(featureFlagHeader), featureFlag)
 }
